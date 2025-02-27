61192c8d6329bc2e938002ff61fbb65d89b8fb6f
JuanDavidDazaR <juan_dav.daza@uao.edu.co>
2025-02-27 18:54:55 -0500

    Resolve merge conflicts

--- src/database/database_create.py
+++ src/database/database_create.py
@@ -1,27 +1,45 @@
-from sqlalchemy import create_engine, text
-from src.database.db_conection import connect_db
+from sqlalchemy import text
+import json
+import os
+from src.database.db_conection import connect_db 
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
+
+with open(CREDENTIALS_PATH, "r", encoding="utf-8") as file:
+    credentials = json.load(file)
+
+db_name = credentials["db_name"]
 
 def create_database():
-    """Crea la base de datos 'candidates' si no existe."""
+    """Crea la base de datos si no existe."""
     try:
         engine = connect_db("postgres")
         with engine.connect() as connection:
             connection.execution_options(isolation_level="AUTOCOMMIT")
 
-            # Verificar si la base de datos ya existe
-            result = connection.execute(text("SELECT 1 FROM pg_database WHERE datname='candidates';")).fetchone()
+            result = connection.execute(text("SELECT 1 FROM pg_database WHERE datname= :db_name;"), {"db_name": db_name}).fetchone()
 
             if not result:
-                connection.execute(text("CREATE DATABASE candidates;"))
-                print("Base de datos 'candidates' creada exitosamente.")
+                connection.execute(text(f'CREATE DATABASE "{db_name}";'))
+                print(f"Base de datos '{db_name}' creada exitosamente.")
             else:
-                print("La base de datos 'candidates' ya existe.")
+                print(f"La base de datos '{db_name}' ya existe.")
 
     except Exception as e:
         print(f"Error al crear la base de datos: {e}")
 
-# Llamar a la función para probar
 if __name__ == "__main__":
+    print("Creando la base de datos...")
     create_database()
+
+
+
+
+
+
+
+
 
 
--- src/database/db_conection.py
+++ src/database/db_conection.py
@@ -2,16 +2,22 @@ from sqlalchemy import create_engine
 import json
 import os
 
+# Obtener credenciales
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
 
-with open(CREDENTIALS_PATH, "r") as file:
+with open(CREDENTIALS_PATH, "r", encoding="utf-8") as file:
     credentials = json.load(file)
 
-def connect_db(db_name="postgres"):
+def connect_db(db_name=None):
     """Conectar a PostgreSQL con SQLAlchemy."""
+    database = db_name if db_name else credentials["db_name"]  # Usa 'postgres' si se pasa un nombre, de lo contrario usa el de credentials
     return create_engine(
-        f"postgresql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{db_name}"
+        f"postgresql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{database}"
     )
+
+
+
+
 
 
